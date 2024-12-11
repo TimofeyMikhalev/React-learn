8982836114628ccc55aef96e9c339a46f183b542
TimofeyMikhalev <79932412+TimofeyMikhalev@users.noreply.github.com>
2024-12-12 00:37:13 +0200

    Checbox React form

--- metrica-react/src/Regestracia.jsx
+++ metrica-react/src/Regestracia.jsx
@@ -2,20 +2,42 @@ import React, {useState} from "react";
 
 
 function Regestration() {
-    const [reg, setReges] = useState('')
+    const [reg, setReges] = useState({
+        name: '',
+        surname: '',
+        tel: '',
+        email: '',
+        password: '',
+        rippasport: ''
+    })
 
-    function handleName(e) {
-        console.log(e.target.value)
-    }
-    function handleSurname(e) {
-        console.log(e.target.value)
-    }
-    function handlePhone(e) {
-        console.log(e.target.value)
-    }
+    function handleGlobal(e) {
+        const { name, value } = e.target
 
-    function displayCheck() {
+        setReges({
+            ...reg,
+            [name]: value
+        })
 
+    }
+    
+    function displayCheck(e) {
+        e.preventDefault()
+
+        for (let key in reg) {
+            if(reg[key].length < 1) {
+                return(alert('Не все поля заполнены'))
+            } 
+            `${key}: ${reg[key]}`
+        }
+
+        if(reg.rippasport === reg.password) {
+            console.log('Пароль сохранен')
+        } else {
+            console.log('Пароль не совпадает')
+        }
+        alert('Круто! Все заполнено')
+        alert(`Name: ${reg.name}, Surname: ${reg.surname}, Phone: ${reg.tel}, E-mail: ${reg.password}`);
     }
 
     return (
@@ -24,20 +46,21 @@ function Regestration() {
             <p>Введите свои данные, чтобы создать аккаунт в сервисе</p>
 
             <form>
-                <input type="text" name="name" id="name" placeholder="Имя" onChange={handleName} />
+                <input type="text" name="name" id="name" placeholder="Имя" onChange={handleGlobal} />
                 <br />
-                <input type="text" name="surname" id="surname" placeholder="Фамилия" onChange={handleSurname} />
+                <input type="text" name="surname" id="surname" placeholder="Фамилия" onChange={handleGlobal} />
                 <br />
-                <input type="tel" name="tel" id="tel" placeholder="Номер телефона" onChange={handlePhone} />
+                <input type="number" name="tel" id="tel" placeholder="Номер телефона" onChange={handleGlobal} />
                 <br />
-                <input type="email" name="email" id="email" placeholder="E-mail" />
+                <input type="email" name="email" id="email" placeholder="E-mail" onChange={handleGlobal}/>
                 <br />
-                <input type="password" name="password" id="password" placeholder="Пароль" />
+                <input type="password" name="password" id="password" placeholder="Пароль" onChange={handleGlobal}/>
                 <br />
-                <input type="password" name="rippasport" id="rippasport" placeholder="Повторный пароль" />
+                <input type="password" name="rippasport" id="rippasport" placeholder="Повторный пароль" onChange={handleGlobal}/>
                 <br />
-                <input type="checkbox" name="myCheckbox" defaultChecked={false} />Подтверждаю пароль
+                <input type="checkbox" name="myCheckbox" onChange={handleGlobal}/>Подтверждаю пароль
                 <br />
+
                 <button type="submit" onClick={displayCheck}>Продолжить</button>
             </form>
         </>
